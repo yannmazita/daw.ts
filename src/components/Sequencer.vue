142c2060078e1355befc4b56c59d10a9a1b007da
Yann Mazita <dev.mazita@gmail.com>
2024-08-16 02:40:54 +0200

    Split sequencer tracks

--- src/components/Sequencer.vue
+++ src/components/Sequencer.vue
@@ -1,25 +1,13 @@
 <template>
     <div id="sequencer-container">
         <SequencerSettings></SequencerSettings>
-        <div id="sequencer-container-tracks" v-for="track in tracks" :key="track.id">
-            <Track :track-id="track.id" :steps="track.steps" />
-        </div>
+        <SequencerTracks></SequencerTracks>
         <SequencerPlaybackControls></SequencerPlaybackControls>
     </div>
 </template>
 
 <script setup lang="ts">
-import { onBeforeUnmount } from 'vue';
-import { storeToRefs } from 'pinia';
-import { useSequencerStore } from '@/stores/sequencerStore.ts';
-import Track from '@/components/Track.vue';
 import SequencerSettings from '@/components/SequencerSettings.vue';
 import SequencerPlaybackControls from '@/components/SequencerPlaybackControls.vue';
-
-const { tracks } = storeToRefs(useSequencerStore());
-
-
-onBeforeUnmount(() => {
-    //sequencerService?.dispose();
-})
+import SequencerTracks from '@/components/SequencerTracks.vue';
 </script>
---
+++ src/components/SequencerTracks.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="sequencer-tracks-container" v-for="track in tracks" :key="track.id">
+        <Track :track-id="track.id" :steps="track.steps" />
+    </div>
+</template>
+<script setup lang="ts">
+import { onBeforeUnmount } from 'vue';
+import { storeToRefs } from 'pinia';
+import { useSequencerStore } from '@/stores/sequencerStore.ts';
+import Track from '@/components/Track.vue';
+
+const { tracks } = storeToRefs(useSequencerStore());
+onBeforeUnmount(() => {
+    //sequencerService?.dispose();
+})
+</script>
