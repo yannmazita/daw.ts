da7a7d1af99630db5bbdbbf6a65513517ca54118
Yann Mazita <dev.mazita@gmail.com>
2024-08-16 02:31:15 +0200

    Split sequencer components

---
+++ src/components/SequencerPlaybackControls.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="sequencer-container-playback" class="flex justify-center">
+        <AppSmallButton @click="sequencerService?.playSequence()">
+            {{ 'Play' }}
+        </AppSmallButton>
+        <AppSmallButton @click="sequencerService?.stopSequence()">
+            {{ 'Stop' }}
+        </AppSmallButton>
+        <AppSmallButton @click="sequencerService?.toggleLoop()">
+            {{ 'Loop' }}
+        </AppSmallButton>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { inject } from 'vue';
+import { sequencerServiceKey } from '@/utils/injection-keys.ts';
+import { SequencerService } from '@/services/SequencerServices.ts';
+import AppSmallButton from '@/components/AppSmallButton.vue';
+
+const sequencerService = inject<SequencerService>(sequencerServiceKey);
+</script>
