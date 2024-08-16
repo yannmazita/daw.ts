dd0e8046aa860de11d815046c5044c06f3d75d77
Yann Mazita <dev.mazita@gmail.com>
2024-08-16 17:33:56 +0200

    Clean up

--- src/components/SequencerTracks.vue
+++ src/components/SequencerTracks.vue
@@ -4,15 +4,11 @@
     </div>
 </template>
 <script setup lang="ts">
-import { onBeforeUnmount, inject, computed } from 'vue';
+import { inject, computed } from 'vue';
 import Track from '@/components/Track.vue';
 import { SequencerService } from '@/services/SequencerServices';
 import { sequencerServiceKey } from '@/utils/injection-keys';
 
 const sequencerService = inject<SequencerService>(sequencerServiceKey);
 const tracks = computed(() => sequencerService?.tracks.value);
-
-onBeforeUnmount(() => {
-    //sequencerService?.dispose();
-})
 </script>
