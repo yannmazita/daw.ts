c4457fa99c168cf17f5a339a22bd2b15ed107375
Yann Mazita <dev.mazita@gmail.com>
2024-08-20 19:11:36 +0200

    Added rudimentary step tracker

--- src/components/Sequencer.vue
+++ src/components/Sequencer.vue
@@ -1,6 +1,7 @@
 <template>
     <div id="sequencer-container">
         <SequencerTrackSettings></SequencerTrackSettings>
+        <SequencerStepTracker></SequencerStepTracker>
         <SequencerSettings></SequencerSettings>
         <SequencerTracks></SequencerTracks>
         <SequencerPlaybackControls></SequencerPlaybackControls>
@@ -12,4 +13,5 @@ import SequencerSettings from '@/components/SequencerSettings.vue';
 import SequencerPlaybackControls from '@/components/SequencerPlaybackControls.vue';
 import SequencerTracks from '@/components/SequencerTracks.vue';
 import SequencerTrackSettings from '@/components/SequencerTrackSettings.vue';
+import SequencerStepTracker from '@/components/SequencerStepTracker.vue';
 </script>
---
+++ src/components/SequencerStepTracker.vue
@@ -0,0 +1,18 @@
+<template>
+    <div v-if="true" id="sequencer-step-tracker-container">
+        <div v-if="true">
+            {{ currentStep }}
+        </div>
+        <AppRangeBar v-model="currentStep" :step="1" :max="numSteps - 1"></AppRangeBar>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { useSequencerStore } from '@/stores/sequencerStore';
+import { storeToRefs } from 'pinia';
+import AppRangeBar from '@/components/AppRangeBar.vue';
+
+const { currentStep, numSteps } = storeToRefs(useSequencerStore());
+
+
+</script>
