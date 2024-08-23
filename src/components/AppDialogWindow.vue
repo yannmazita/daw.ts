facedfa6a6b5f43422d138c18541f5ba88c854cc
Yann Mazita <dev.mazita@gmail.com>
2024-08-23 16:19:45 +0200

    typo fix

--- src/components/AppDialogWindow.vue
+++ src/components/AppDialogWindow.vue
@@ -14,7 +14,7 @@
     </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { useDialogStore } from '@/stores/dialogStore';
 import { computed, nextTick, watch, onMounted } from 'vue';
 
