bdf985b6361ec4013d7c540773b00decebc065d4
Yann Mazita <dev.mazita@gmail.com>
2024-10-11 17:03:54 +0200

    Added min value

--- src/components/AppRangeBar.vue
+++ src/components/AppRangeBar.vue
@@ -1,14 +1,17 @@
 <template>
     <div id="app-range-bar-container" class="flex">
-        <input v-model="model" type="range" class="range [--range-shdw:#5bcefa]" :step="props.step" :max="props.max">
+        <input v-model="model" type="range" class="range [--range-shdw:#5bcefa]" :step="props.step" :min="props.min"
+            :max="props.max">
     </div>
 </template>
 <script setup lang="ts">
 interface Props {
+    min?: number;
     max?: number;
     step?: number;
 }
 const props = withDefaults(defineProps<Props>(), {
+    min: () => 0,
     max: () => 100,
     step: () => 1,
 });
