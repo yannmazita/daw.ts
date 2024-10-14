51f95221306d28c3ed845fb35a62ebc60caa92d2
Yann Mazita <dev.mazita@gmail.com>
2024-10-14 23:27:45 +0200

    Pulsating play button and pause button update

--- src/components/SequencerPlaybackControls.vue
+++ src/components/SequencerPlaybackControls.vue
@@ -1,11 +1,10 @@
 <template>
     <div id="sequencer-container-playback" class="flex flex-col items-center space-y-4">
         <div class="flex space-x-2">
-            <AppSmallButton @click="playbackManager.playSequence()">
+            <AppSmallButton @click="playbackManager.playSequence()" :class="{ 'pulse-animation': isPlaying }">
                 Play
             </AppSmallButton>
-            <AppSmallButton @click="playbackManager.pauseSequence()"
-                :disabled="playbackManager.getStatus() === SequenceStatus.Paused">
+            <AppSmallButton @click="playbackManager.pauseSequence()" :disabled="disablePause">
                 Pause
             </AppSmallButton>
             <AppSmallButton @click="playbackManager.stopSequence()">
@@ -19,7 +18,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, inject, watch } from 'vue';
+import { ref, inject, watch, computed } from 'vue';
 import { sequencerPlaybackManagerKey } from '@/utils/injection-keys';
 import { SequencerPlaybackManager } from '@/services/SequencerPlaybackManager';
 import AppSmallButton from '@/components/AppSmallButton.vue';
@@ -30,7 +29,30 @@ const playbackManager = inject(sequencerPlaybackManagerKey) as SequencerPlayback
 
 const loopEnabled = ref(playbackManager.loopEnabled);
 
+const isPlaying = computed(() => playbackManager.getStatus() === SequenceStatus.Playing);
+const disablePause = computed(() => (playbackManager.getStatus() === SequenceStatus.Paused) || (playbackManager.getStatus() === SequenceStatus.Stopped));
+
 watch(loopEnabled, (newValue) => {
     playbackManager.loopEnabled = newValue;
 });
 </script>
+
+<style scoped>
+@keyframes pulse {
+    0% {
+        transform: scale(1);
+    }
+
+    50% {
+        transform: scale(1.05);
+    }
+
+    100% {
+        transform: scale(1);
+    }
+}
+
+.pulse-animation {
+    animation: pulse 1s infinite;
+}
+</style>
