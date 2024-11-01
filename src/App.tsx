db4646f2a2c6110888da3bc588306f95d4c11660
Yann Mazita <dev.mazita@gmail.com>
2024-11-01 18:30:31 +0100

    Updated main entry points

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="h-screen w-full overflow-hidden">
+      Hello world!
+    </div>
+  );
+};
+
+export default App;
