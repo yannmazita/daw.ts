5a87fe3d45433e847069b0e64648a7d37b094462
Yann Mazita <dev.mazita@gmail.com>
2024-11-03 21:35:30 +0100

    Updated command manager

---
+++ src/common/services/commandManagerInstance.ts
@@ -0,0 +1,8 @@
+// src/common/services/commandManagerInstance.ts
+
+import { CommandManager } from './CommandManager';
+
+/**
+ * Singleton instance of CommandManager to be used throughout the application.
+ */
+export const commandManager = new CommandManager();
