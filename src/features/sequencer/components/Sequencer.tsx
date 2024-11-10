f6d261a83ff8962925b5877ff904c170b91d4534
Yann Mazita <dev.mazita@gmail.com>
2024-11-10 01:22:00 +0100

    Added initializer function

--- src/features/sequencer/components/Sequencer.tsx
+++ src/features/sequencer/components/Sequencer.tsx
@@ -1,14 +1,22 @@
 // src/features/components/Sequencer.tsx
 
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import SequencerTracks from './SequencerTracks';
 import SequencerTrackSettings from './SequencerTrackSettings';
+import { initializeSequencer } from '../slices/sequencerSlice';
 
 const Sequencer: React.FC = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(initializeSequencer(4)); // Initialize with 4 tracks
+  }, [dispatch]);
+
   return (
     <div id='sequencer-wrapper' className='grid grid-cols-3'>
-        <SequencerTrackSettings className='col-span-1'/>
-        <SequencerTracks className='col-span-2'/>
+      <SequencerTrackSettings className='col-span-1' />
+      <SequencerTracks className='col-span-2' />
     </div>
   );
 };
