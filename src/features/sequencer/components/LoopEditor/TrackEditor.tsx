a72c3baa2c123d2f28ea4f2fb36d8272a0770563
Yann Mazita <dev.mazita@gmail.com>
2024-11-27 18:15:58 +0100

    prettier

--- src/features/sequencer/components/LoopEditor/TrackEditor.tsx
+++ src/features/sequencer/components/LoopEditor/TrackEditor.tsx
@@ -1,23 +1,28 @@
 // src/features/sequencer/components/LoopEditor/TrackEditor.tsx
 
-import React from 'react';
-import { SequencerTrackInfo } from '@/core/interfaces/sequencer';
-import StepGrid from './StepGrid';
-import LoopControls from './LoopControls';
+import React from "react";
+import { SequencerTrackInfo } from "@/core/interfaces/sequencer";
+import StepGrid from "./StepGrid";
+import LoopControls from "./LoopControls";
 
 interface TrackEditorProps {
   trackInfo: SequencerTrackInfo;
   displayedSteps: number;
 }
 
-const TrackEditor: React.FC<TrackEditorProps> = React.memo(({ trackInfo, displayedSteps }) => {
-  return (
-    <>
-      <h4 className="font-semibold mb-2">Track {trackInfo.trackIndex + 1}</h4>
-      <LoopControls trackInfo={trackInfo} displayedSteps={displayedSteps} />
-      <StepGrid trackIndex={trackInfo.trackIndex} displayedSteps={displayedSteps} />
-    </>
-  );
-});
+const TrackEditor: React.FC<TrackEditorProps> = React.memo(
+  ({ trackInfo, displayedSteps }) => {
+    return (
+      <>
+        <h4 className="font-semibold mb-2">Track {trackInfo.trackIndex + 1}</h4>
+        <LoopControls trackInfo={trackInfo} displayedSteps={displayedSteps} />
+        <StepGrid
+          trackIndex={trackInfo.trackIndex}
+          displayedSteps={displayedSteps}
+        />
+      </>
+    );
+  },
+);
 
 export default TrackEditor;
