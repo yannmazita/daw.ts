83a8889277d8181fbae888fa63556d824454c52f
Yann Mazita <dev.mazita@gmail.com>
2024-11-28 08:57:41 +0100

    Added mixer to main view

--- src/views/MainView.tsx
+++ src/views/MainView.tsx
@@ -2,11 +2,13 @@
 
 import React from "react";
 import Sequencer from "@/features/sequencer/components/Sequencer";
+import Mixer from "@/features/mixer/components/Mixer";
 
 const MainView: React.FC = () => {
   return (
-    <div className="flex justify-center">
+    <div className="flex flex-col gap-4 p-4">
       <Sequencer />
+      <Mixer />
     </div>
   );
 };
