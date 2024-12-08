2d7fbbb9b91ff50973e65adeace0c58aa58fc52a
Yann Mazita <dev.mazita@gmail.com>
2024-12-08 06:53:20 +0100

    Removed sequencer path

--- src/features/sequencer/hooks/playback/index.ts
+++
@@ -1,8 +0,0 @@
-// src/features/sequencer/hooks/playback/index.ts
-
-export { usePlaybackController } from "./usePlaybackController";
-export { useEventScheduler } from "./useEventScheduler";
-export { useTrackScheduler } from "./useTrackScheduler";
-export { useAutomationScheduler } from "./useAutomationScheduler";
-export { useTimingController } from "./useTimingController";
-export type { PlaybackControllerConfig } from "./usePlaybackController";
