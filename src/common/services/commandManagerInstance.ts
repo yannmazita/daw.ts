939100a6f54e325dfa7afcb974730bcb1450ffb8
Yann Mazita <dev.mazita@gmail.com>
2024-12-10 15:02:04 +0100

    Removed anything related to command management

--- src/common/services/commandManagerInstance.ts
+++
@@ -1,8 +0,0 @@
-// src/common/services/commandManagerInstance.ts
-
-import { CommandManager } from "./CommandManager";
-
-/**
- * Singleton instance of CommandManager to be used throughout the application.
- */
-export const commandManager = new CommandManager();
