f7ac4575403df0af7680bf2542563bcc3c4c1af4
Yann Mazita <dev.mazita@gmail.com>
2024-12-14 01:34:22 +0100

    Updated toolbar layout

--- src/common/components/Toolbar/Toolbar.tsx
+++ src/common/components/Toolbar/Toolbar.tsx
@@ -4,9 +4,11 @@ import { PlaybackControls } from "./PlaybackControls/PlaybackControls";
 
 export const Toolbar = () => {
   return (
-    <div className="flex items-center justify-between border-b border-border px-4">
+    <div className="flex items-center justify-between border-b border-border px-4 dark:border-b dark:border-border">
       <ViewSelector />
-      <PlaybackControls />
+      <div className="flex flex-grow justify-center">
+        <PlaybackControls />
+      </div>
     </div>
   );
 };
