616bfef569a7da20b2cbc36610a3caf0c9f0322d
Yann Mazita <dev.mazita@gmail.com>
2025-01-04 09:31:51 +0100

    Show devices in device tab

--- src/common/components/DetailView.tsx
+++ src/common/components/DetailView.tsx
@@ -5,8 +5,21 @@ import {
   TabsList,
   TabsTrigger,
 } from "@/common/shadcn/ui/tabs";
+import { useEngineStore } from "@/core/stores/useEngineStore";
+import { DeviceUnit } from "@/features/mix/components/MixerControls/DeviceUnit";
 
 export const DetailView: React.FC = () => {
+  const { devices, mixerTracks, soundChains } = useEngineStore(
+    (state) => state.mix,
+  );
+  const parentId = "master"; // hardcoded for now
+  const mixerTrack = mixerTracks[parentId];
+  const soundChain = soundChains[parentId];
+  const deviceIds = mixerTrack?.deviceIds ?? soundChain?.deviceIds ?? [];
+  const filteredDevices = Object.values(devices).filter(
+    (device) => device.parentId === parentId,
+  );
+
   return (
     <div className="col-span-12 flex flex-col border border-border">
       <Tabs defaultValue="devices" className="h-full">
@@ -16,7 +29,11 @@ export const DetailView: React.FC = () => {
           <TabsTrigger value="clip">Clip</TabsTrigger>
         </TabsList>
         <TabsContent value="devices" className="h-full">
-          <div className="p-4">Device View</div>
+          <div className="flex flex-row gap-2 p-4">
+            {filteredDevices.map((device) => (
+              <DeviceUnit key={device.id} device={device} />
+            ))}
+          </div>
         </TabsContent>
         <TabsContent value="automation" className="h-full">
           <div className="p-4">Automation View</div>
