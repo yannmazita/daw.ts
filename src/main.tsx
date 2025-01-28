8c3dd25ac0e63add4fd751c6cc8e363cfc6c75ab
Yann Mazita <dev.mazita@gmail.com>
2025-01-28 19:03:56 +0100

    Added react scan

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { scan } from "react-scan";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
@@ -7,6 +8,13 @@ const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
 
 const root = createRoot(rootElement);
+
+if (typeof window !== "undefined") {
+  scan({
+    enabled: true,
+    log: false,
+  });
+}
 
 root.render(
   <StrictMode>
